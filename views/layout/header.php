7b7d1bdb545ecfb84bd4d6110dd3a289ea4bd43a
Joel Neskenz <joelneskenz@gmail.com>
2025-10-30 18:50:38 -0500

    change

--- views/layout/header.php
+++ views/layout/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD Trabajadores</title>
+    <title>Mantenedor Trabajadores</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body { padding-top: 70px; }
